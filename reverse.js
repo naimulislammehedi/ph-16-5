d78acb95b402fdab1cac29bdc1f23379d7ed73d3
Mehedi <mehedi.interface@gmail.com>
2026-04-28 11:48:36 +0600

    array with/without reverse method

--- reverse.js
+++ reverse.js
@@ -1,23 +1,48 @@
-const numbers = [22, 33, 44, 55, 66];
+// using built-in reverse() (fasted, but with a catch)
+const arr = [10, 20, 30, 40, 50];
 
-numbers.reverse();
+console.log(arr);
+arr.reverse(); 
+console.log(arr);
 
-console.log(numbers);
+// hidden risk 
+const original = [1, 2, 3, 4, 5];
+const reversed = original.reverse(); 
+console.log(reversed); 
+console.log(original);
 
-const reversed = numbers.reverse(); 
-console.log(reversed);
+// keep the original array unchanged
+const origanl2 = [...original].reverse(); 
+console.log(origanl2);
 
-const rev_numbers = []
+// without reverse() (manual approach - this is what matters)
+// two pointer swap 
+const arr3 = [10, 20, 30, 40, 50];
 
-for (const num of numbers) {
-    // console.log(num);
-    rev_numbers.unshift(num);
+let left = 0; 
+let right = arr.length - 1; 
+
+while (left < right) {
+    // swap 
+    [arr3[left], arr3[right]] = [arr3[right], arr3[left]];
+    left++;
+    right--;
+}
+console.log(arr3);
+
+const arr4 = [10, 20, 30, 40, 50];
+const reversedArr4 = [];
+
+for (let i = arr4.length - 1; i >= 0; i--) {
+    reversedArr4.push(arr4[i]); 
 }
 
-console.log(rev_numbers);
+console.log(reversedArr4);
 
-const reversed_numbers = []; 
-for (let i = 0; i < numbers.length; i++) {
-    const num = numbers[i]; 
-    reversed_numbers.unshift(num);
+const arr5 = [10, 20, 30, 40, 50];
+const reversedArr5 = []; 
+
+for (let i = 0; i < arr5.length; i++) {
+    reversedArr5.unshift(arr5[i]); 
 }
+console.log(reversedArr5);
